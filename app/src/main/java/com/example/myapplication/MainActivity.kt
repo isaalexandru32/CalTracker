75a5d8e8fc716ad816df4f8dfd3b3e05898f1661
Huzum Eduard-Stefan <galied25@yahoo.com>
2025-09-19 18:46:06 +0300

    you can now add custom food items

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,26 +1,71 @@
 package com.example.myapplication
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
-import com.example.myapplication.databinding.ActivityMainBinding
 import android.widget.Button
+import androidx.appcompat.app.AlertDialog
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 
 class MainActivity : AppCompatActivity() , View.OnClickListener {
 
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: foodAdapter
+    private val foodList = mutableListOf<food_item>()
 
-//test pull ghghgghfghfghfghdfghj
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-    }
+        recyclerView = findViewById(R.id.foodList)
+        val btAddFood: Button = findViewById(R.id.btAddFood)
 
-}
+        adapter = foodAdapter(foodList)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = adapter
+
+        btAddFood.setOnClickListener {
+            showAddFoodDialog()
+        }
+
+    }
 
     override fun onClick(p0: View?) {
+        TODO("Not yet implemented")
     }
+
+    private fun showAddFoodDialog(){
+        val dialogView = layoutInflater.inflate(R.layout.dialog_edit_food, null)
+
+        val etTitle = dialogView.findViewById<EditText>(R.id.etTitle)
+        val etCal = dialogView.findViewById<EditText>(R.id.etCal)
+        val etProt = dialogView.findViewById<EditText>(R.id.etProt)
+        val etFats = dialogView.findViewById<EditText>(R.id.etFats)
+        val etCarbs = dialogView.findViewById<EditText>(R.id.etCarbs)
+
+        AlertDialog.Builder(this)
+            .setTitle("Add an element")
+            .setView(dialogView)
+            .setPositiveButton("Add") { _, _ ->
+                val newFood = food_item(
+                    title = etTitle.text.toString(),
+                    cal = etCal.text.toString(),
+                    prot = etProt.text.toString(),
+                    fats = etFats.text.toString(),
+                    carbs = etCarbs.text.toString()
+                )
+
+                foodList.add(newFood)
+
+                adapter.notifyItemInserted(foodList.size - 1)
+                recyclerView.scrollToPosition(foodList.size - 1)
+            }
+            .setNegativeButton("Cancel", null)
+            .show()
+    }
+
 }
+
--- app/src/main/java/com/example/myapplication/foodAdapter.kt
+++ app/src/main/java/com/example/myapplication/foodAdapter.kt
@@ -4,7 +4,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.view.menu.MenuView
 import androidx.recyclerview.widget.RecyclerView
 
@@ -33,7 +35,58 @@ class foodAdapter(
     }
 
     override fun onBindViewHolder(holder: foodViewHolder, position: Int) {
-        R.layout.consumed_food
+        val currentFood = foods[position]
+
+        //add the values of food_item in the text view
+        holder.tvitem_food.text = currentFood.title
+        holder.tvitem_cal.text = currentFood.cal
+        holder.tvitem_prot.text = currentFood.prot
+        holder.tvitem_fats.text = currentFood.fats
+        holder.tvitem_carb.text = currentFood.carbs
+
+        holder.btDeleteFood.setOnClickListener {
+            foods.removeAt(position)
+            notifyItemRemoved(position) //update UI
+        }
+
+        holder.btModFood.setOnClickListener {
+            //this is where we will modify the added food
+
+            val context = holder.itemView.context
+            val dialogView = LayoutInflater.from(context).inflate(R.layout.dialog_edit_food, null)
+
+            val etTitle = dialogView.findViewById<EditText>(R.id.etTitle)
+            val etCal = dialogView.findViewById<EditText>(R.id.etCal)
+            val etProt = dialogView.findViewById<EditText>(R.id.etProt)
+            val etFats = dialogView.findViewById<EditText>(R.id.etFats)
+            val etCarbs = dialogView.findViewById<EditText>(R.id.etCarbs)
+
+            etTitle.setText(currentFood.title)
+            etCal.setText(currentFood.cal)
+            etProt.setText(currentFood.prot)
+            etFats.setText(currentFood.fats)
+            etCarbs.setText(currentFood.carbs)
+
+            AlertDialog.Builder(context)
+                .setTitle("Modify an item")
+                .setView(dialogView)
+                .setPositiveButton("Save") { _, _ ->
+                    // create a new object with the updated values
+                    val updatedFood = currentFood.copy(
+                        title = etTitle.text.toString(),
+                        cal = etCal.text.toString(),
+                        prot = etProt.text.toString(),
+                        fats = etFats.text.toString(),
+                        carbs = etCarbs.text.toString()
+                    )
+
+                    foods[position] = updatedFood
+
+                    notifyItemChanged(position)
+                }
+                .setNegativeButton("Cancel", null)
+                .show()
+        }
     }
 
     override fun getItemCount(): Int {
